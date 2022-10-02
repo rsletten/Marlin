c1dcd413a3856c21501d7113f7c365f79fb34029
Kris Kersey <kkersey@dispersivegroup.com>
2022-10-01 21:49:19 -0400

    Fixed left-front tramming point.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1019,7 +1019,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probe points.
-  #define TRAMMING_POINT_XY { {  50, 310 }, { 310,  50 }, { 310, 310 }, { 50, 310 } }
+  #define TRAMMING_POINT_XY { {  50, 50 }, { 310,  50 }, { 310, 310 }, { 50, 310 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
